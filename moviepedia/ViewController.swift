e1d3c1fc04ebcd1a0f7e5dcabe8e614118e93743
Lucas Souza <lss8@cin.ufpe.br>
2022-07-05 19:01:13 -0300

    Adicao da base da interface do projeto

---
+++ moviepedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviepedia
+//
+//  Created by lss8 on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
